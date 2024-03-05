595ca6c3d26a16a4d89dd0488e544f40fabdbc4b
AyaAlaa <Ayaalaa010920@gmail.com>
2024-03-05 13:38:44 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApplication());
+}
+
+
+
+class MyApplication extends StatelessWidget {
+  const MyApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+
+    );
+  }
+}
+
